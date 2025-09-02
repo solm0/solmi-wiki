104aba188a3f447f4b910264fe5478ee139503be
정솔미 <solmi-@kookmin.ac.kr>
2025-09-03 02:16:16 +0900

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -73,7 +73,7 @@ export default async function RootLayout({
 
           <InternalLinkTooltip />
 
-          <Alert />
+          {/* <Alert /> */}
         </ThemeProvider>
       </body>
     </html>
