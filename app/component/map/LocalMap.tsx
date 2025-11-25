9cbeb88e09a1266aebfee267258cd30245db58b0
정솔미 <solmi-@kookmin.ac.kr>
2025-11-25 16:25:17 +0900

    osrm 두개의 위치 연결

--- app/component/map/LocalMap.tsx
+++ app/component/map/LocalMap.tsx
@@ -2,7 +2,7 @@
 
 import Map, { Layer, Popup, Source, MapRef } from 'react-map-gl/maplibre';
 import 'maplibre-gl/dist/maplibre-gl.css';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import { Place } from '@/app/lib/type';
 import { NoPost } from '../hyperlink-map/ToolBox';
 import type {
@@ -18,12 +18,14 @@ export default function LocalMap({
 }: {
   places?: Place[];
 }) {
+  // 디폴트 위치
   const hochschuleKempten = {
     longitude: Number(places?.[0]?.lng) ?? 10.313611,
     latitude: Number(places?.[0]?.lat) ?? 47.715833,
     zoom: 16
   }
 
+  // 호버, 클릭 관련
   const [viewState, setViewState] = useState(hochschuleKempten);
   const [hoveredId, setHoveredId] = useState<string | null>(null);
 
@@ -45,29 +47,63 @@ export default function LocalMap({
     });
   }, [clickedId]);
 
-  if (!places || places.length === 0) return <NoPost text='선택된 글 없음 또는 이 글에 장소'/>
+  // 마커 데이터 만들기
   
-  const placeData = places.map((place, i) => 
-    ({
-      type: 'Feature',
-      geometry: {
-        type: "Point",
-        coordinates: [Number(place.lng), Number(place.lat)]
-      },
-      properties: {
-        i: i+1,
-        id: place.id,
-        name: place.name,
-      }
-    }),
-  )
   
-  const geojson: FeatureCollection = {
+  const placeData = useMemo(() => {
+    if (!places) return [];
+    return places.map((place, i) => 
+      ({
+        type: 'Feature',
+        geometry: {
+          type: "Point",
+          coordinates: [Number(place.lng), Number(place.lat)]
+        },
+        properties: {
+          i: i+1,
+          id: place.id,
+          name: place.name,
+        }
+      }),
+    )
+  }, [places]);
+  
+  const geojson = useMemo<FeatureCollection>(() => ({
     type: 'FeatureCollection',
     features: placeData as Feature<Geometry, GeoJsonProperties>[]
+  }), [placeData]);
+
+  // 동선 데이터 만들기
+  const [route, setRoute] = useState<Feature<Geometry, GeoJsonProperties> | null>(null);
+
+  async function fetchRoute(start: [number, number], end: [number, number]) {
+    const res = await fetch(
+      `https://router.project-osrm.org/route/v1/driving/${start[0]},${start[1]};${end[0]},${end[1]}?overview=full&geometries=geojson`
+    );
+    const data = await res.json();
+
+    const line = data.routes[0].geometry; // LineString
+
+    setRoute({
+      type: "Feature",
+      geometry: line,
+      properties: {}
+    });
   }
 
-  if (places) return (
+  useEffect(() => {
+    if (!placeData[0] || !placeData[1]) return;
+
+    fetchRoute(
+      placeData[0].geometry.coordinates as [number, number],
+      placeData[1].geometry.coordinates as [number, number]
+    );
+  }, [placeData]);
+
+  
+
+  if (!places || places.length === 0) return <NoPost text='선택된 글 없음 또는 이 글에 장소'/>
+  else return (
     <Map
       ref={mapRef}
       {...viewState}
@@ -86,6 +122,7 @@ export default function LocalMap({
       mapStyle={`https://api.maptiler.com/maps/019aafb1-6648-769c-889b-369294610c89/style.json?key=5WUNujbtty6Dt1NUzT6r`}
     >
       <Source id='marker-layer' type='geojson' data={geojson}>
+        {/* 마커 레이어 */}
         <Layer
           id='marker'
           type='circle'
@@ -104,6 +141,8 @@ export default function LocalMap({
             'circle-stroke-width': 0.5
           }}
         />
+
+        {/* 마커 위 인덱스 레이어 */}
         <Layer
           id='marker-labels'
           type='symbol'
@@ -118,6 +157,22 @@ export default function LocalMap({
           }}
         />
       </Source>
+      {route &&
+        <Source id='route' type='geojson' data={route}>
+          {/* 동선 레이어 */}
+          <Layer
+            id='route-line'
+            type='line'
+            source='route'
+            paint={{
+              "line-width": 4,
+              "line-color": "#ff0000"
+            }}
+          />
+
+          {/* 동선 위 진행도 레이어 */}
+        </Source>
+      }
 
       {hoveredId && (
         <Popup
